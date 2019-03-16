4c44f3b5ee0e69ba9d2f2033913f1d99d0293cd1
Gabby <salufie@gmail.com>
2019-03-16 22:51:03 +1100

    Changing file structure

---
+++ Unihack2019/ViewController/PointsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PointsViewController.swift
+//  Unihack2019
+//
+//  Created by Gabrielle Chandrasaputra on 16/3/19.
+//  Copyright © 2019 Gabrielle Chandrasaputra. All rights reserved.
+//
+
+import Foundation
