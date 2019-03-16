987045657fc948d1beaad615485cb23ec9e21857
Gabby <salufie@gmail.com>
2019-03-17 06:12:56 +1100

    My profile working

--- Unihack2019/ProfileViewController.swift
+++ Unihack2019/ProfileViewController.swift
@@ -8,37 +8,37 @@
 
 import UIKit
 
-class ProfileiewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var options = ["Change Location", "Set Alarm", "Settings"]
-    
-    
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let list = ["NAME", "EMAIL", "CURRENT CREDIT", "TOTAL CREDIT", "CLAIMED CREDIT"]
+    let name = ["REBECCA CHEUNG", "", "", "", ""]
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.backgroundColor = UIColor.clear
         tableView.delegate = self
         tableView.dataSource = self
 }
-
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return options.count
+        return list.count
     }
-
+    
     //set table view data cells
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-
+        
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = options[indexPath.row] //list array as table cells
+        cell.textLabel?.text = list[indexPath.row] + " : " + name[indexPath.row]//list array as table cells
         cell.backgroundColor = UIColor.clear //make cells clear
         cell.textLabel?.textColor = UIColor.white //make text color white
         return(cell)
-
+        
     }
+
     
     
 }
