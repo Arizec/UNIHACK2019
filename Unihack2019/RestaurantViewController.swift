4d4549af857b88abd06bdfe33cc78ba45bd68613
Gabby <salufie@gmail.com>
2019-03-16 19:55:34 +1100

    Latest changes

---
+++ Unihack2019/RestaurantViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RestaurantViewController.swift
+//  Unihack2019
+//
+//  Created by Gabrielle Chandrasaputra on 16/3/19.
+//  Copyright © 2019 Gabrielle Chandrasaputra. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
